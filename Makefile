21e5bdf23806478dad19980560edaff094b00636
javiercarrascocruz <javier.carrasco.cruz@gmail.com>
2020-12-23 00:21:02 +0100

    ejemplo 4 con funciones en init y exit usando identificadores init

--- Makefile
+++ Makefile
@@ -1,9 +1,17 @@
+#obj-m indica el objeto módulo que se va a crear.
+# := es asignación e invalida asignaciones anteriores.
+# += incrementa la asignación para crear más módulos.
+
 obj-m := ex01_simple_module.o
 obj-m += ex02_simple_module_init.o
 obj-m += ex03_simple_module_exit.o
 
+# asignación que invalida lo anterior.
 obj-m := ex0203_simple_module.o
+#módulo compuesto de dos objetos, se indica así (los dos objetos se crean
+#automáticamente al compilar de sus respectivos códigos fuente)
 ex0203_simple_module-objs := ex02_simple_module_init.o ex03_simple_module_exit.o
 
-obj-m := ex04_simple_module.o
-ex04_simple_module.objs := ex02_simple_module_init-o ex03_simple_module_exit.o
+#asignación que invalida lo anterior.
+obj-m := ex01_simple_module.o
+obj-m += ex04_simple_module.o
--- ex04_simple_module.c
+++ ex04_simple_module.c
@@ -1,17 +1,48 @@
+/*
+  Dos temas importantes aquí:
+  1.-licencia de módulo (evitar dudas sobre su posible origen malware)
+  2.- __init/__initdata para indicar al kernel que las funciones y variables con
+  dichos indicadores se pueden eliminar de RAM tras la inicialización.
+  Nótese que function2 no lleva __init porque también se llama desde la función
+  de salida y por tanto debe seguir presente en RAM tras la inicialización.
+  Como function1 solo se llama desde la función de init, se puede usar __init.
+*/
 #include <linux/init.h>
 #include <linux/module.h>
 
 MODULE_LICENSE("GPL");
 
-__init int ex04_simple_module_init(void)
+__init int ex04_simple_module_function1(void)
 {
   printk(KERN_ALERT "Inside the %s function\n", __FUNCTION__);
+}
+
+int ex04_simple_module_function2(void)
+{
+  printk(KERN_ALERT "Inside the %s function\n", __FUNCTION__);
+}
+
+__initdata int count = 5;
+
+__init int ex04_simple_module_init(void)
+{
+  int index;
+  printk(KERN_ALERT "Inside the %s function\n", __FUNCTION__);
+  for(index = 0; index < count; index++)
+  {
+    printk(KERN_ALERT "Index = %d\n", index);
+  }
+
+  ex04_simple_module_function1();
+  ex04_simple_module_function2();
   return 0;
 }
 
 void ex04_simple_module_exit(void)
 {
   printk(KERN_ALERT "Inside the %s function\n", __FUNCTION__);
+
+  ex04_simple_module_function2();
 }
 
 module_init(ex04_simple_module_init);
